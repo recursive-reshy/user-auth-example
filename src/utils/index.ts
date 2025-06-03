51fcd7461c6cfea70073ee150d47d2cfae38fc5a
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-03 17:12:33 +0800

    src\utils\index.ts: Fixed type error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,7 +5,7 @@ const random = () => crypto.randomBytes( 128 ).toString( 'base64' )
 
 const authentication = ( salt: string, password: string ) =>
   crypto.createHmac( 'sha256', [ salt, password ].join( '/' ) ) // Create a hash of the salt and password
-    .update( process.env.SECRET || '' ) // Update the hash with the secret
+    .update( process.env.SECRET as string ) // Update the hash with the secret
     .digest( 'hex' ) // Digest the hash
 
 export {
