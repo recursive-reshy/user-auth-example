8e0ded8d1e132417af8e62db0c528fd9f9ab641c
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-03 15:32:13 +0800

    src\controllers\authentication.ts: Added authenticator controller

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from 'express'
+
+import { random, authentication } from '../utils/index.js'
+import asyncWrapper from '../middleware/asyncWrapper.js'
+
+import { createUser, getUserByEmail } from '../models/Users.js'
+
+const signup = asyncWrapper( async ( request: Request, response: Response ) => {
+  const { username, email, password } = request.body
+
+  if ( !username || !email || !password ) {
+    return response.sendStatus( 400 )
+  }
+
+  const existingUser = await getUserByEmail( email )
+
+  if( existingUser ) {
+    return response.sendStatus( 400 )
+  }
+
+  const salt = random()
+
+  const user = await createUser( { 
+    username,
+    email,
+    authentication: {
+      salt,
+      password: authentication( salt, password ),
+    }
+  } )
+
+  return response.status( 200 ).json( user ).end() // end() is used to end the response
+} )
+
+const login = asyncWrapper( async ( request: Request, response: Response ) => {
+  const { email, password } = request.body
+
+  if( !email || !password ) {
+    return response.sendStatus( 400 )
+  }
+
+  const user = await getUserByEmail( email )
+    .select( '+authentication.salt +authentication.password' ) // select is used to select the fields that are not included in the response
+
+  if( !user || !user.authentication ) {
+    return response.sendStatus( 400 )
+  }
+
+  const expectedHash = authentication( user.authentication.salt, password )
+
+  if( expectedHash != user.authentication.password ) {
+    return response.sendStatus( 403 )
+  }
+
+  user.authentication.sessionToken = authentication( random(), user._id.toString() )
+
+  await user.save()
+
+  response.cookie( 
+    'NARESH_AUTH', // name of the cookie
+    user.authentication.sessionToken, // value of the cookie
+    { domain: 'localhost', // domain of the cookie
+      path: '/' // path of the cookie
+    }
+  )
+
+  return response.status( 200 ).json( user ).end()
+} )
+
+export { signup, login }
