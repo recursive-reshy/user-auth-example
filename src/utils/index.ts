9f83de928b13c156ca09f2d39fb4505429d276d5
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-03 14:20:55 +0800

    src\utils\index.ts: Added helper functions

---
+++ src/utils/index.ts
@@ -0,0 +1,14 @@
+import crypto from 'crypto'
+
+// Generate a random string
+const random = () => crypto.randomBytes( 128 ).toString( 'base64' )
+
+const authentication = ( salt: string, password: string ) =>
+  crypto.createHmac( 'sha256', [ salt, password ].join( '/' ) ) // Create a hash of the salt and password
+    .update( process.env.SECRET || '' ) // Update the hash with the secret
+    .digest( 'hex' ) // Digest the hash
+
+export {
+  random,
+  authentication
+}
